a764ff8f2853302a1aabed220d2d622e5ee6485c
tinyafuro <tinyforest.win@gmail.com>
2018-10-11 21:51:17 +0900

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,35 +2,25 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-
   #rootへのGetレスポンステスト
-  test "should get root" do
+  test "should get home" do
     get root_url
     assert_response :success;
-  end
-
-  #static/homeへのGetレスポンステスト
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Home"
   end
 
   #static/helpへのGetレスポンステスト
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help"
   end
 
   #static/aboutへのGetレスポンステスト
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About"
   end
 
 end
