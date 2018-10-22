28914c13603944ddfa5461627e39257f9f3ef19c
tinyafuro <tinyforest.win@gmail.com>
2018-10-22 17:45:31 +0900

    Task list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,11 @@
 class TasksController < ApplicationController
-  before_action :logged_in_user,  only: [:create, :destroy]
+  before_action :logged_in_user,  only: [:index, :create, :destroy]
   before_action :correct_user,    only: :destroy
+
+  #すべてのユーザータスク一覧
+  def index
+    @tasks = Task.paginate(page: params[:page])
+  end
 
   def create
     @task = current_user.tasks.build(task_params)
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
-  resources :tasks, only: [:create, :destroy]
+  resources :tasks, only: [:index, :create, :destroy]
   
 end
